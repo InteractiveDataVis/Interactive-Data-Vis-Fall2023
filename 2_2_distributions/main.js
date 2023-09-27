caba9f6d68e43fa66b9152308f8306c73231f838
Ellie Frymire <ellie.frymire@gmail.com>
2023-09-26 17:30:17 -0700

    week 4

--- 2_2_distributions/main.js
+++ 2_2_distributions/main.js
@@ -1,16 +1,77 @@
 /* CONSTANTS AND GLOBALS */
-// const width = ,
-//   height = ,
-//   margin = ,
-//   radius = ;
+const width = window.innerWidth * 0.7,
+  height = window.innerHeight * 0.7,
+  margin = { top: 20, bottom: 60, left: 60, right: 40 },
+  radius = 5;
 
 /* LOAD DATA */
-d3.csv("../data/MoMA_distributions.csv", d3.autoType)
-  .then(data => {
-    console.log(data)
+d3.json("../data/environmentRatings.json", d3.autoType).then(data => {
+  console.log(data)
 
-    /* SCALES */
+  /* SCALES */
+  const xScale = d3.scaleLinear()
+    .domain([0, 1])
+    .range([margin.left, width - margin.right])
+
+  const yScale = d3.scaleLinear()
+    .domain([0, 100])
+    .range([height - margin.bottom, margin.top])
+
+
+  const xAxis = d3.axisBottom(xScale)
+  const yAxis = d3.axisLeft(yScale)
+
+  /* HTML ELEMENTS */
+  // svg
+  const svg = d3.select("#container")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+
+  // axis scales
+
+  svg.append("g")
+    .attr("transform", `translate(${0},${height - margin.bottom})`)
+    .call(selection => selection.call(xAxis))
+
+  svg.append("g")
+    .attr("transform", `translate(${margin.left},${0})`)
+    .call(selection => selection.call(yAxis))
+ 
+  // debugging
+  // console.log(data.find(d => d.BioID === "G000560"))
+
+  // circles
+  // svg.selectAll("circle.senator")
+  //   .data(data, d => d.BioID)
+  //   .join(
+  //     enter => enter.append("circle")
+  //       .attr("class", "senator")
+  //       .attr("id", d => d.BioID)
+  //       .attr("r", 0)
+  //       .attr("cx", d => xScale(d.ideologyScore2020))
+  //       .attr("cy", d => yScale(d.envScore2020))
+  //       .call(sel => sel.transition()
+  //         .duration(1000)
+  //         .delay((d, i) => d.ideologyScore2020 * 1000)
+  //         .attr("r", 5))
+  //     ,
+  //     update => update,
+  //     exit => exit.remove()
+  //   )
+
+  svg.selectAll("circle.senator")
+    .data(data, d => d.BioID)
+    .join("circle")
+    .attr("class", "senator")
+    .attr("id", d => d.BioID)
+    .attr("r", 0)
+    .attr("cx", d => xScale(d.ideologyScore2020))
+    .attr("cy", d => yScale(d.envScore2020))
+    .transition()
+      .duration(1000)
+      .delay((d, i) => d.ideologyScore2020 * 1000)
+      .attr("r", 5)
     
-    /* HTML ELEMENTS */
-    
-  });
+
+});
--- 2_3_time_series/main.js
+++ 2_3_time_series/main.js
@@ -1,26 +1,72 @@
- /* CONSTANTS AND GLOBALS */
-// const width = ,
-//   height = ,
-//   margin = ;
+/* CONSTANTS AND GLOBALS */
+const width = window.innerWidth * 0.7,
+height = window.innerHeight * 0.7,
+margin = { top: 20, bottom: 50, left: 100, right: 60 }
 
 /* LOAD DATA */
-d3.csv('[PATH_TO_YOUR_DATA]', d => {
+d3.csv('../data/populationOverTime.csv', d => {
   return {
-    // year: new Date(+d.Year, 0, 1),
-    // country: d.Entity,
-    // population: +d.Population
+    year: new Date(+d.Year, 0, 1),
+    rawYear: +d.Year,
+    country: d.Entity,
+    population: +d.Population
   }
 }).then(data => {
-  console.log('data :>> ', data);
+console.log('data :>> ', data);
 
-  // SCALES
+// SCALES
 
-  // CREATE SVG ELEMENT
+const xScale = d3.scaleTime()
+  // .domain(d3.extent(data, d => d.year))
+  .domain(d3.extent(data, d => d.rawYear))
+  .range([margin.left, width-margin.right])
 
-  // BUILD AND CALL AXES
+const yScale = d3.scaleLinear()
+  .domain(d3.extent(data, d => d.population))
+  .range([height - margin.bottom, margin.top])
 
-  // LINE GENERATOR FUNCTION
+// CREATE SVG ELEMENT
+const svg = d3.select("#container")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height)
 
-  // DRAW LINE
+// BUILD AND CALL AXES
+
+const xAxis = d3.axisBottom(xScale)
+  .ticks(6) // limit the number of tick marks showing -- note: this is approximate
+
+const xAxisGroup = svg.append("g")
+  .attr("class", "xAxis")
+  .attr("transform", `translate(${0}, ${height - margin.bottom})`)
+  .call(xAxis)
+
+const yAxis = d3.axisLeft(yScale)
+  // .tickFormat(formatBillions)
+
+const yAxisGroup = svg.append("g")
+  .attr("class", "yAxis")
+  .attr("transform", `translate(${margin.left}, ${0})`)
+  .call(yAxis)
+
+// LINE GENERATOR FUNCTION
+
+const lineGen = d3.line()
+  .x(d => xScale(d.rawYear))
+  .y(d => yScale(d.population))
+
+// GROUP THE DATA BY COUNTRY
+const groupedData = d3.groups(data, d => d.country)
+console.log('groupedData', groupedData)
+
+// DRAW LINE
+svg.selectAll("path")
+  .data(groupedData)
+  .join("path")
+  // .attr("d", d => lineGen(d[1]))
+  .attr("d", ([countryName, data]) => lineGen(data))
+  .attr("id", d => d[0])
+  .style("fill", "none")
+  .style("stroke", "black")
 
 });
